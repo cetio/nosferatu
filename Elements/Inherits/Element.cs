287e27a0df3320dddf906c0f352bffdcd1ce27ac
stake <stake@MKMKMK0>
2022-11-18 18:23:11 -0500

    Add project files.

---
+++ Elements/Inherits/Element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nosferatu.Elements.Inherits
+{
+    public class Element
+    {
+        protected Point Position;
+        protected Size Size;
+        protected string? Name;
+        protected Element? Parent;
+        protected List<Element>? Children;
+    }
+}
---
+++ Elements/Input.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nosferatu.Elements
+{
+    public class Input
+    {
+    }
+}
